bc92f0c953aef0da634ee67fd328d8159d200ac4
AbdiSarbessa <https://github.com/AbdiSerbessa/codeops-practice.git>
2026-08-26 21:28:48 +0300

    chore: remove components folder from github

--- Module-03/day-26/src/components/Dish/Dish.jsx
+++
@@ -1,9 +0,0 @@
-import './Dish.css'
-export default function Dish({ name, price }) {
-  return (
-    <div className="dish-card">
-      <h3 className="dish-name">{name}</h3>
-      <span className="dish-price">${price.toFixed(2)}</span>
-    </div>
-  );
-}
